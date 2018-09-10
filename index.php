20246bad1e8008a8c325b570b36bf3c96a4de433
Kert Liinat <kertliinat@users.noreply.github.com>
2018-09-10 10:40:53 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> Kert Liinat, Õpilane </title>
+  
+</head>
+<body>
+	<h1>Liinat</h1>
+	<p>Siin on <a href="http://www.tlu.ee" target="_blank"> TLÜ</a> õppetöö raames valminud veebilehed.
+	Lisasin selle lause kodutöö raames.
+	Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+	<!--<img src = "http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ terra õppehoone">-->
+	<img src = "../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ terra õppehoone">
+	<P> Mul on sõber Künter Evert, kes teeb <a href="../../~kunteve" target="_blank">veebi</a>. </p>
+</body>
+</html>
