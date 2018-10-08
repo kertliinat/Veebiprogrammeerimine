5d4773851ff625fd9b065db57d23d057ffbddbba
Kert Liinat <kertliinat@users.noreply.github.com>
2018-10-08 11:45:55 +0300

    kuuenda tunni jama

---
+++ tund6/functions.php
@@ -0,0 +1,146 @@
+<?php
+require("../../../config.php");
+$database = "if18_kert_li_1";
+
+//alustan sessiooni
+session_start();
+
+function readallunvalidatedmessages(){
+	$notice = "<ul> \n";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT id, message FROM vpamsg WHERE valid IS NULL ORDER BY id DESC");
+	echo $mysqli->error;
+	$stmt->bind_result($id, $msg);
+	$stmt->execute();
+	
+	while($stmt->fetch()){
+		$notice .= "<li>" .$msg .'<br><a href="validatemessage.php?id=' .$id .'">Valideeri</a>' ."</li> \n";
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+
+function signin($email, $password) {
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT id, firstname, lastname, password FROM vpusers WHERE email=?");
+	echo $mysqli->error;
+	$stmt->bind_param("s", $email);
+	$stmt->bind_result($idFromDb, $firstnameFromDb, $lastnameFromDb, $passwordFromDb);
+	if($stmt->execute()){
+		//kui päring õnnestus
+		if($stmt->fetch()) {
+			//kasutaja on olemas
+			if(password_verify($password, $passwordFromDb)){
+				//kui salasõna klapib
+				$notice = "Logisite sisse!";
+				//määran sessioonimuutujad
+				$_SESSION["userId"] = $idFromDb;
+				$_SESSION["userFirstName"] = $firstnameFromDb;
+				$_SESSION["userLastName"] = $lastnameFromDb;
+				$_SESSION["useremail"] = $email;
+				//liigume kohe vaid sisselogitutele mõeldud pealehele
+				$stmt->close();
+				$mysqli->close();
+				header("Location: main.php");
+				exit();
+			}else {
+				$notice = "Vale salasõna!";
+			}
+		} else {
+			$notice = "Sellist kasutajat (" .$email .") ei leitud!";
+		}
+	}else {
+		$notice= "Sisselogimisel tekkis tehniline viga!".$stmt->error;
+	}
+	
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+}//sisselogimine lõppeb
+  
+  
+  //kasutaja salvestamine
+  function signup($name, $surname, $email, $gender, $birthDate, $password){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	//kontrollime, ega kasutajat juba olemas pole
+	$stmt = $mysqli->prepare("SELECT id FROM vpusers WHERE email=?");
+	echo $mysqli->error;
+	$stmt->bind_param("s",$email);
+	$stmt->execute();
+	if($stmt->fetch()){
+		//leiti selline, seega ei saa uut salvestada
+		$notice = "Sellise kasutajatunnusega (" .$email .") kasutaja on juba olemas! Uut kasutajat ei salvestatud!";
+	} else {
+		$stmt->close();
+		$stmt = $mysqli->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES(?,?,?,?,?,?)");
+    	echo $mysqli->error;
+	    $options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
+	    $pwdhash = password_hash($password, PASSWORD_BCRYPT, $options);
+	    $stmt->bind_param("sssiss", $name, $surname, $birthDate, $gender, $email, $pwdhash);
+	    if($stmt->execute()){
+		  $notice = "ok";
+	    } else {
+	      $notice = "error" .$stmt->error;	
+	    }
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  function saveAMsg($msg){
+    $notice = ""; //see on teade, mis antakse salvestamise kohta
+    $mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+    $stmt = $mysqli->prepare("INSERT INTO vpamsg (message) VALUES(?)");
+    echo $mysqli->error;
+    $stmt->bind_param("s", $msg);//s - string, i - integer, d - decimal
+    if ($stmt->execute()){
+      $notice = 'Sõnum: "' .$msg .'" on salvestatud!';  
+    } else {
+	  $notice = "Sõnumi salvestamisel tekkis tõrge: " .$stmt->error;
+    }
+    $stmt->close();
+    $mysqli->close();
+    return $notice;
+  }
+  //funktsioon, mis loeb kõiki salvestatud sõnumeid (seda kutsub readmsg.php)
+  function readallmessages(){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT message FROM vpamsg3");
+	echo $mysqli->error;
+	$stmt->bind_result($msg);
+	$stmt->execute();
+	while($stmt->fetch()){
+		$notice .= "<p>" .$msg ."</p> \n";
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+  function readmsgforvalidation($editId){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT message FROM vpamsg WHERE id = ?");
+	$stmt->bind_param("i", $editId);
+	$stmt->bind_result($msg);
+	$stmt->execute();
+	if($stmt->fetch()){
+		$notice = $msg;
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+   
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?>
---
+++ tund6/main.php
@@ -0,0 +1,34 @@
+<?php
+  require("functions.php");
+  //kui pole sisse loginud
+  
+  //kui pole sisselogitud
+  if(!isset($_SESSION["userId"])){
+	  header("Location: index_3.php");
+	  exit();
+  }
+  //väljalogimine
+  if(isset($_GET["logout"])) {
+	  session_destroy();
+	  header("Location:index_3.php");
+	  exit();
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Pealeht</title>
+  </head>
+  <body>
+    <h1>Pealeht</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+	<hr>
+	<p> Olete sisse loginud nimega: <?php echo $_SESSION["userFirstName"]. " " .$_SESSION["userLastName"]; ?>. <b><a href="?logout=1">Logi välja</a></b></p>
+	<ul>
+	  <li> Valideeri anonüümseid <a href ="validatemsg.php">sõnumeid</a></li>
+	</ul>
+	
+  </body>
+</html>
